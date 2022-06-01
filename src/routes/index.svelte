e14f192d1ccc1dd6ae485673472a621f4ddbb32e
xancti <aeonar@live.com>
2022-06-01 18:28:52 +0800

    First commit: done with tutorial and some extras

---
+++ src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script>
+    import '../main.scss'
+    import image1 from '$lib/images/image1.jpg';
+    import image2 from '$lib/images/image2.jpg';
+    import image3 from '$lib/images/image3.jpg';
+    import image4 from '$lib/images/image4.jpg';
+    import image5 from '$lib/images/image5.jpg';
+</script>
+
+<body>
+
+    <div class="container">
+        <div class="feature">
+            <img src={image2} alt="Terrain" class="feature-img">
+            <div class="content">
+                <h3>An Amazing Experience</h3>
+                <a href="#" class="cta">Learn more</a>
+            </div>
+        </div>
+        <div class="feature">
+            <img src={image3} alt="Terrain" class="feature-img">
+            <div class="content">
+                <h3>An Amazing Experience</h3>
+                <a href="#" class="cta">Learn more</a>
+            </div>
+        </div>
+        <div class="feature">
+            <img src={image4} alt="Terrain" class="feature-img">
+            <div class="content">
+                <h3>An Amazing Experience</h3>
+                <a href="#" class="cta">Learn more</a>
+            </div>
+        </div>
+        <div class="feature">
+            <img src={image5} alt="Terrain" class="feature-img">
+            <div class="content">
+                <h3>An Amazing Experience</h3>
+                <a href="#" class="cta">Learn more</a>
+            </div>
+        </div>
+    </div>
+
+</body>
